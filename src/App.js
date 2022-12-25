e3e60ee4cb8effcaf3cd570fb6886ed6419f8b62
pranav s <dev.pranabsk@gmail.com>
2022-12-25 16:03:52 +0530

    Account+Login+Signup Component Setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './ContextAPI/AuthContext';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/Login/SignUp';
+import Account from './components/Account/Account';
 
 function App() {
   return (
     <AuthContextProvider>
-      <Navbar />
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/home" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/account" element={<Account />}/>
       </Routes>
     </Router>
     </AuthContextProvider>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../media/images/Logo.png';
 
 const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4 w-full z-[100] absolute">
-            <img className="w-32 cursor-pointer" src={Logo} alt=""/>
+            <Link to="/home">
+            <img className="w-32" src={Logo} alt="Logo"/>
+            </Link>
+
             <div>
-                <button className="text-white pr-4">Sign In</button>
-                <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Sign Up</button>
+            <Link to='/login'>
+            <button className="text-white pr-4">Sign In</button>
+            </Link>
+            <Link to='/signup'>
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Sign Up</button>
+            </Link>
             </div>
         </div>
     );
