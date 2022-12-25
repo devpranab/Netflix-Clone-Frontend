215723855b1265d61960d69c4fa24263cf2d6740
pranav s <dev.pranabsk@gmail.com>
2022-12-25 14:57:36 +0530

    Add Side Arrow Button

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import axios from "axios";
 import Movie from "./Movie";
 
@@ -10,18 +11,39 @@ const Row = ({ title, fetchURL, rowID }) => {
       setMovies(response.data.results);
     });
   }, [fetchURL]);
+  console.log(movies);
+
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <div>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-      <div
-          id={'slider' + rowID}
-          className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
-      {movies.map((item, id) => (
+          {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block"
+          size={40}
+        />
       </div>
     </div>
   );
