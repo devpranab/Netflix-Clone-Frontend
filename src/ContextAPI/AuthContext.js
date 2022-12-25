87805b0691c981f68efb2ded9f28d1aa462b9314
pranav s <dev.pranabsk@gmail.com>
2022-12-25 15:35:27 +0530

    firebase.config.js file path change

--- src/ContextAPI/AuthContext.js
+++ src/ContextAPI/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { auth } from '../../firebase.config';
+import { auth } from '../firebase.config';
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
