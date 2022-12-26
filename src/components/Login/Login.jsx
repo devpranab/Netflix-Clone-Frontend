e4233299a696fa87136068a0ef025ff2c3849f44
pranav s <dev.pranabsk@gmail.com>
2022-12-26 10:47:26 +0530

    Error Cond Func. in Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,17 +5,19 @@ import { UserAuth } from '../../ContextAPI/AuthContext';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    //const [error, setError] = useState('');
+    const [error, setError] = useState('');
     const { user, logIn } = UserAuth();
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setError('');
         try {
           await logIn(email, password)
           navigate('/')
         } catch (error) {
           console.log(error);
+          setError(error.message);
         }
       };
 
@@ -34,6 +36,7 @@ const Login = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Login</h1>
+              {error ? <p className='p-3 bg-red-400 my-2'>!!{error}</p> : null}
               <form
                 onSubmit={handleSubmit}
                 className='w-full flex flex-col py-4'
